bb42c28ca7561aba9649be4bb39ecb399611c040
DESKTOP-L0J2R0E\imgur <imgurveer@gmail.com>
2024-08-08 00:34:44 +1000

    temporary change

--- backend/db.js
+++ backend/db.js
@@ -3,21 +3,22 @@ import env from "dotenv";
 
 env.config();
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-        require: true, // This will help you. But you will see nwe error
-        rejectUnauthorized: false // This line will fix new error
-        }
-    },
-});
 try{
+    const db = new pg.Client({
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
+        port: process.env.PG_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+            }
+        },
+    });
+
     db.connect();
 } catch(err) {
     console.log(err);
