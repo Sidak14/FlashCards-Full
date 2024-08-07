437d0b965e9fc24a70205c5fb8afa9424f881a9d
DESKTOP-L0J2R0E\imgur <imgurveer@gmail.com>
2024-08-07 23:46:08 +1000

    temporary change for digitalocean

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,14 @@ import env from "dotenv";
 
 env.config();
 
-const db = new pg.Client({
+/*const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
 });
-db.connect();
+db.connect();*/
+const db = "dummy";
 
 export default db;
