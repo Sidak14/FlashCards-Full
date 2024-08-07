f968a50d0d649917836d76b8e64508f91ddc0d9e
DESKTOP-L0J2R0E\imgur <imgurveer@gmail.com>
2024-08-08 01:03:31 +1000

    temp

--- backend/db.js
+++ backend/db.js
@@ -3,28 +3,23 @@ import env from "dotenv";
 
 env.config();
 
-let db = "dummy";
-try{
-    db = new pg.Client({
-        user: process.env.PG_USER,
-        host: process.env.PG_HOST,
-        database: process.env.PG_DATABASE,
-        password: process.env.PG_PASSWORD,
-        port: process.env.PG_PORT,
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-            require: true, // This will help you. But you will see nwe error
-            rejectUnauthorized: false // This line will fix new error
-            }
-        },
-    });
 
-    db.connect();
-} catch(err) {
-    console.log(err);
-    console.log(process.env.PG_USER, process.env.PG_HOST, process.env.PG_DATABASE, process.env.PG_PASSWORD, process.env.PG_PORT, process.env.PG_CA_CERT);
-}
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+        }
+    },
+});
+
+await db.connect();
 
 
 export default db;
